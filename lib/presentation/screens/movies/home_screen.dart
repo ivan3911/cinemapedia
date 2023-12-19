e284ee5fc6d66b21d531df78a35ba11725f2e5a9
Demet <demet2824@gmail.com>
2023-12-19 13:43:08 -0600

    implementation swipercard

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,6 @@
 
 import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -40,15 +41,27 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider );
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text( movie.title),
-        );
+    return Column(
+      children: [
+
+        const CustomAppbar(),
         
-      },
-      );
+        MoviesSlideshow(movies: nowPlayingMovies)
+        // Expanded(
+        //   child: ListView.builder(
+        //       itemCount: nowPlayingMovies.length,
+        //       itemBuilder: (context, index) {
+        //   final movie = nowPlayingMovies[index];
+        //   return ListTile(
+        //     title: Text( movie.title),
+        //   );
+          
+        //       },
+        //       ),
+        // )
+
+
+      ],
+    );
   }
 }
