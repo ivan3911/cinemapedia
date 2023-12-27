d2177d5771e58da2bb424b6db5c617ebf1643626
Demet <demet2824@gmail.com>
2023-12-26 19:14:25 -0600

    horizontal list view done

--- lib/presentation/providers/movies/movies_providers.dart
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -16,6 +16,7 @@ typedef MovieCallback = Future<List<Movie>> Function({ int page });
 class MoviesNotifier extends StateNotifier<List<Movie>> {
 
   int currentPage = 0;
+  bool isLoading = false;
   MovieCallback fetchMoreMovies;
 
   MoviesNotifier({
@@ -23,9 +24,15 @@ class MoviesNotifier extends StateNotifier<List<Movie>> {
   }): super([]);
 
   Future<void> loadNextPage() async {
+    if ( isLoading ) return;
+    isLoading = true;
+
     currentPage++;
     final List<Movie> movies = await fetchMoreMovies(page: currentPage);
     state = [...state, ...movies];
+    
+    await Future.delayed(const Duration( milliseconds: 300));
+    isLoading = false;
   }
 
 }
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -55,8 +55,10 @@ class _HomeViewState extends ConsumerState<_HomeView> {
           movies: nowPlayingMovies,
           title: 'En cines', 
           subTitle: 'Lunes 20',
-        )
-        
+          loadNextPage: () => ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage()
+        ),
+
+       
       ],
     );
   }
